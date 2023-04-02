fdf523afa8e9dc64db25b2d6d6de53e806e09fad
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-01 18:54:14 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(miles){
   // Write your code here!
+ 
+if (miles <= 400){
+  return "This one is on me!"
+}
+else if (miles > 400 && miles < 2000){
+  return "That will be twenty bucks."
+}
+else if (miles > 2000 && miles < 2500) {
+  return "I will gladly take your thirty bucks.";
+}
+else if (miles > 2500) {
+  return "No can do."
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+const decision = city == "NYC" ?  "Ok, sounds good." : "No go.";
+ return decision;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    break;
+    case "not as generous":
+      return "Thank you.";
+    break; 
+    default:
+      return "Bye.";
+    break; 
+  }
 }
